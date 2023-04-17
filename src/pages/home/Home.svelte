a59b8393ec968f2a3ddc49afdaae0dd4e5fed067
wolfderechter <wolfderechter@gmail.com>
2023-04-17 16:07:31 +0200

    Add interval and progressbar

--- src/pages/home/Home.svelte
+++ src/pages/home/Home.svelte
@@ -11,7 +11,7 @@
   import { getNextBlockId } from "../../utils/getNextBlockId";
   import { getGasPrice } from "../../utils/getGasPrice";
   import { getQueuedTransactions } from "../../utils/getQueuedTransactions";
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
   import { getProofReward } from "../../utils/getProofReward";
   import type { Status, StatusIndicatorProp } from "../../domain/status";
   import { getStateVariables } from "../../utils/getStateVariables";
@@ -20,6 +20,7 @@
   import DetailsModal from "../../components/DetailsModal.svelte";
   import { addressSubsection } from "../../utils/addressSubsection";
   import Web3 from "web3";
+  import ProgressBar from "progressbar.js";
   // import Prover from "src/components/details/Prover.svelte";
   // import Node from "src/components/details/Node.svelte";
   // import Proposer from "src/components/details/Proposer.svelte";
@@ -49,10 +50,11 @@
   let peers = 0;
   let blockNumber;
   let syncingStatus;
-  let syncingProgress;
+  let syncingProgress = 0;
   let L1Balance;
   let L2Balance;
   let nodeType = nodeTypes.Node;
+  let interval: NodeJS.Timer;
 
   const myNode = new Web3("http://localhost:8545");
   const taikoL2 = new Web3("https://l2rpc.a2.taiko.xyz");
@@ -82,25 +84,36 @@
     console.log(await taikoL2.eth.getNodeInfo());
   }
   onMount(async () => {
-    try {
-      fetchMetric();
-      let response = await fetch(
-        "http://localhost:9090/api/v1/query?query=p2p_peers"
-      );
-      let data = await response.json();
-      let value = data.data.result[0].value[1];
-      peers = value;
-    } catch (e) {
-      console.error(e);
-    }
+    // Interval to fetch metrics every second
+    interval = setInterval(async () => {
+      try {
+        // fetchMetric();
+
+        // let response = await fetch(
+        //   "http://localhost:9090/api/v1/query?query=p2p_peers"
+        // );
+        // let data = await response.json();
+        // let value = data.data.result[0].value[1];
+        // peers = value;
+
+        if (syncingProgress < 100) {
+          syncingProgress++;
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }, 100);
+  });
+
+  onDestroy(() => {
+    if (interval) clearInterval(interval);
   });
 </script>
 
 <div class="flex flex-col items-center py-6">
   <div class="text-center">
     <img src="./src/assets/TaikoLogo.png" alt="" width="350px" />
-
-    <div class="block">
+    <div class="block nodeTypes flex justify-evenly mt-4">
       <button
         class:active={nodeType === nodeTypes.Node}
         on:click={() => (nodeType = nodeTypes.Node)}>Node</button
@@ -115,6 +128,18 @@
         on:click={() => (nodeType = nodeTypes.Prover)}>Prover</button
       >
     </div>
+  </div>
+  <!-- Progress Bar -->
+  <!-- <div id="progressParent" class="pt-5">
+    <div class="progress-bar" style={`width: ${syncingProgress}%`} />
+  </div> -->
+  <div class="progress-bar m-4">
+    <div class="progress-bar__background">
+      <div class="progress-bar__text">
+        {syncingProgress < 100 ? syncingProgress + "%" : "Synced!"}
+      </div>
+    </div>
+    <div class="progress-bar__fill" style={`width: ${syncingProgress}%`} />
   </div>
 
   <!-- Temporary generic metrics -->
@@ -132,7 +157,7 @@
         Progress: {syncingProgress.toFixed(2)}%
       </p>
     {:else}
-      <p>Synced!</p>
+      <!-- <p>Synced!</p> -->
     {/if}
     {#if L1Balance && L2Balance}
       <p>L1 Balance: {L1Balance.toFixed(6)} ETH</p>
@@ -151,10 +176,57 @@
 </div>
 
 <style>
-  button.active {
-    border: 1px solid #fc07c1;
+  .nodeTypes {
+    color: gray;
+    font-weight: lighter;
+  }
+
+  .nodeTypes .active {
+    border: 1px solid #ff9fe9;
     border-radius: 5px;
-    background-color: #fc07c1;
+    background-color: #ff9fe9;
     color: white;
+    font-weight: bold;
+    padding: 1px 10px;
+  }
+
+  .progress-bar {
+    width: 200px;
+    height: 20px;
+    border-radius: 10px;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .progress-bar__background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #e0e0e0;
+    border-radius: 10px;
+  }
+
+  .progress-bar__fill {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    background-color: #ff9fe9;
+    border-radius: 10px;
+    transition: width 0.2s ease-in-out;
+    width: 0%;
+    z-index: 1;
+  }
+
+  .progress-bar__text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 12px;
+    color: white;
+    z-index: 2;
   }
 </style>
