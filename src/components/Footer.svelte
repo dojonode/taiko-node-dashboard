1a9a25724dcb2467f865ce102e175acbaa2f2a4b
wolfez <wolfderechter@gmail.com>
2023-12-20 21:23:55 +0100

    update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,42 +1,37 @@
-<script lang="ts">
-  import TaikoHeart from "../assets/TaikoHeart.png";
-
+<script context="module">
+	import TaikoHeart from '../assets/TaikoHeart.png';
+	import GithubIcon from '../assets/icons/Github.png';
+	import TwitterIcon from '../assets/icons/Twitter.png';
+	import DiscordIcon from '../assets/icons/Discord.png';
 </script>
 
-<div class="footer text-center relative md:top-[35px] top-[0px] bottom-1 mb-1">
-  <p>
-    made with <img
-    class="w-3 inline align-baseline"
-    src={TaikoHeart}
-    alt="Taiko heart icon"
-    /> by
-  </p>
-  <p>wolfez and helios</p>
-  <div class="flex items-center justify-center mt-2">
+<div class="footer relative md:top-[35px] top-[0px] bottom-1 mb-1">
+  <div class="flex items-center justify-center mb-2 gap-4">
+    <a href="https://twitter.com/dojonode" target="_blank">
+      <img src={TwitterIcon} class="icon" alt="twitter icon" />
+    </a>
     <a href="https://github.com/wolfderechter/taiko-node-dashboard" target="_blank">
-      <span class="github-icon">
-        <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="18"
-        height="18"
-        fill="currentColor"
-        class="bi bi-github"
-        viewBox="0 0 16 16"
-        >
-        <path
-        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.20-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.20-.82 2.20-.82.44 1.10.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.20 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-        />
-      </svg>
-    </span>
+      <img src={GithubIcon} class="icon" alt="github icon" />
+    </a>
+    <a href="https://discord.gg/ThvjqUWWkz" target="_blank">
+      <img src={DiscordIcon} class="icon" alt="discord icon" />
     </a>
   </div>
+	<p>
+		made with <img class="w-4 inline align-sub" src={TaikoHeart} alt="Taiko heart icon" /> by
+	</p>
+	<p>wolfez and helios</p>
 </div>
 
-
 <style>
-  .footer {
-    color: hsl(var(--twc-textColor));
-    text-align: center;
-    padding: 20px;
+	.footer {
+		color: hsl(var(--twc-cardSubBodyColor));
+		text-align: center;
+		padding: 20px;
+		padding-bottom: 40px;
+	}
+
+  .icon{
+    width: 20px;
   }
 </style>
