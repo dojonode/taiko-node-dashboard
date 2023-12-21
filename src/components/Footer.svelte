7fb16f217c5cbf252ac185e0eb7f80e7d4fa9d42
wolfez <wolfderechter@gmail.com>
2023-12-21 19:39:24 +0100

    update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -10,10 +10,10 @@
     <a href="https://twitter.com/dojonode" target="_blank">
       <img src={TwitterIcon} class="icon" alt="twitter icon" />
     </a>
-    <a href="https://github.com/wolfderechter/taiko-node-dashboard" target="_blank">
+    <a href="https://github.com/dojonode/taiko-node-dashboard" target="_blank">
       <img src={GithubIcon} class="icon" alt="github icon" />
     </a>
-    <a href="https://discord.gg/ThvjqUWWkz" target="_blank">
+    <a href="https://discord.gg/taikoxyz" target="_blank">
       <img src={DiscordIcon} class="icon" alt="discord icon" />
     </a>
   </div>
