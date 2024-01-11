4786432786a5f6c336b94798ae39b7c75646a275
wolfez <wolfderechter@gmail.com>
2024-01-11 16:29:30 +0100

    update tailwind theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,22 +4,6 @@ export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   plugins: [
     createThemes({
-      original: {
-        primaryColor: "#e81899", //taiko pink
-        secondaryColor: "#ff9fe9", //taiko soft pink
-        tertiaryColor: "#808080", //gray color
-        backgroundColor: "#ffffff", //main background
-        progressBarTextColor: "#808080",
-        progressBarBackgroundColor: "#e0e0e0",
-        cardBackgroundColor: "#f5f5f5", //card background
-        cardTitleColor: "#e81899",
-        cardBodyTextColor: "#6b6b6b",
-        cardSubBodyColor: "#b3b3b3",
-        settingsBtnColor: "#9baab2",
-        settingsInputBackgroundColor: "#ebebeb",
-        settingsPrimaryTextColor: "#000", // black
-        settingsSecondaryTextColor: "#374151", // blueish gray color
-      },
       paper: {
         primaryColor: "#e81899",
         secondaryColor: "#f8c4cf",
@@ -29,11 +13,12 @@ export default {
         nodeTypesColorActive: "#E81899",
         progressBarFillColor: "#F8C4CF",
         progressBarBackgroundColor: "#E0E7EC",
-        cardBackgroundColor: "#f5f5f5",
+        cardBackgroundColor: "#F4F4F4",
         cardTitleColor: "#E81899",
         cardSubBodyColor: "#9A989F",
-        settingsAccentColor: "#E0E7EC",
-        settingsBtnColor: "#9baab2",
+        inputAccentColor: "#E0E7EC",
+        glowColor: "#0000004C",
+        themeSwitcherColor: "#F4AB9F",
       },
       light: {
         primaryColor: "#e81899",
@@ -44,11 +29,12 @@ export default {
         nodeTypesColorActive: "#E81899",
         progressBarFillColor: "#F8C4CF",
         progressBarBackgroundColor: "#E0E7EC",
-        cardBackgroundColor: "#f5f5f5",
+        cardBackgroundColor: "#F4F4F4",
         cardTitleColor: "#E81899",
         cardSubBodyColor: "#9A989F",
-        settingsAccentColor: "#E0E7EC",
-        settingsBtnColor: "#9baab2",
+        inputAccentColor: "#E0E7EC",
+        glowColor: "#0000004C",
+        themeSwitcherColor: "#F4AB9F",
       },
       dark: {
         primaryColor: "#e81899",
@@ -62,9 +48,10 @@ export default {
         cardBackgroundColor: "#444444",
         cardTitleColor: "#ec4899",
         cardSubBodyColor: "#BDC0BA",
-        settingsAccentColor: "#707D75",
-        settingsBtnColor: "#9baab2",
+        inputAccentColor: "#707D75",
+        glowColor: "#ffffffb3",
+        themeSwitcherColor: "#F4AB9F",
       },
-    }),
+    })
   ],
 };
